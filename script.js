0aba348c522caa6952889d642dfe78ca49cbc678
Sara Shipley <sarashipley1@gmail.com>
2025-01-11 23:46:59 +0000

    set up pub list in js and define marker function.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+//List pubs JSON
+const pubs = [
+    {
+        title: "Lucky Saint Pub",
+        lat: 51.5,
+        lon: -0.14,
+        rating: 3,
+    },
+    {
+        title: "Redemption Bar",
+        lat: 51.5,
+        lon: -0.13,
+        rating: 3,
+    }
 
-<!-- JavaScript -->
+]
 
     // Initialize the map
     const map = L.map('map');
@@ -10,3 +24,16 @@
         maxZoom: 19,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    //L.marker([51.522220250000004,-0.1441884678939735]).addTo(map);
+    // copy these 
+ // lucky saint pub 	51.522220250000004,-0.1441884678939735
+
+ //Defining a function
+ function makeMarkers(markerData) {
+    //do stuff to each pub.
+    markerData.forEach((pub) => { 
+        L.marker([pub.lat, pub.lon]).addTo(map);
+    })
+ }
+ makeMarkers(pubs)
