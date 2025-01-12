a4e3936f79c7945a42a9546a2ca845185892cc54
Sara Shipley <sarashipley1@gmail.com>
2025-01-12 09:33:15 +0000

    add more pub loaction markers to the list.

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ const pubs = [
         title: "Lucky Saint Pub",
         lat: 51.5,
         lon: -0.14,
-        rating: 3,
+        rating: 5,
     },
     {
         title: "Redemption Bar",
         lat: 51.5,
         lon: -0.13,
         rating: 3,
+    },
+    {
+        title: "Club Soda Tasting Rooms",
+        lat: 51.5,
+        lon: -0.12,
+        rating: 5,
+    },
+
+    {
+        title: "Mikkeller Bar",
+        lat: 51.5,
+        lon: -0.07,
+        rating: 4,
     }
 
 ]
@@ -28,12 +41,21 @@ const pubs = [
 
  //Defining a function
  function makeMarkers(markerData) {
-    //do stuff to each pub.
+    //amends info for each pub
     markerData.forEach((pub) => { 
+        const popupHtml = `
+        <strong>${pub.title}</strong>
+        <p>Rating:${pub.rating}/5 </p>
+        `
         const marker =  L.marker([pub.lat, pub.lon]);
         marker.addTo(map);
-        marker.bindPopup("Hello");
+        marker.bindPopup(popupHtml);
 
     })
  }
  makeMarkers(pubs)
+//Creation steps
+ //1.Define array of pubs using json, included title, location (lat/long) and rating score.
+ //2.defined function for the markers, created my own function to iterate over the pubs.
+ //3.for each pub in the function wrote html for it line 38 apply to map 39 bind html into pop up
+ //line 43 calling the function
